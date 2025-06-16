5bc459a74b3c817d8c247b03fd8887ca78873ee2
Johan Ekman <37696604+MrShtekman@users.noreply.github.com>
2025-06-16 16:35:43 +0200

    Add cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import MainRouter from "./routes/index";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
